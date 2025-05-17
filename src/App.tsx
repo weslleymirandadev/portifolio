d8941873a41433d22f53b45afa2449418786fabe
weslleymirandadev <contato@weslleymiranda.dev>
2025-05-17 03:15:19 -0300

    feat(main): added main page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BlackHole } from "./components/universe/BlackHole";
+import { FromShellToName } from "./components/whoami/FromShellToName";
+
+export default function App() {
+  return (
+    <>
+      <FromShellToName />
+      <BlackHole />
+    </>
+  );
+}
