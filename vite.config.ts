7533709b6423b6592a8e54dbfd3af5a0a546116c
weslleymirandadev <contato@weslleymiranda.dev>
2025-05-20 14:00:00 +0100

    feat(dependencies): added new dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    glsl({
+      include: ["**/*.glsl", "**/*.vs", "**/*.fs"],
+      defaultExtension: "glsl",
+    }),
+  ],
 });
