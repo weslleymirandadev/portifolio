6482299b3c59ead76fbfc19f04291b5bf478ed85
weslleymirandadev <contato@weslleymiranda.dev>
2025-05-21 16:43:50 -0300

    feat(utils): improved 3D points distribution

--- src/utils/3D_Gradients.ts
+++ src/utils/3D_Gradients.ts
@@ -3,45 +3,42 @@ const MAX_RADIUS = 15;
 const DEPTH = 2;
 const NUM_POINTS = 2500;
 
-/**
- * --- Credit ---
- * https://stackoverflow.com/questions/16360533/calculate-color-hex-having-2-colors-and-percent-position
- */
-
-const randomFromInterval = (min: number, max: number) => {
+const randomFromInterval = (min: number, max: number): number => {
   return Math.random() * (max - min) + min;
 };
 
-export const pointsInner = Array.from(
+export interface PointData {
+  position: [number, number, number];
+}
+
+export const pointsInner: PointData[] = Array.from(
   { length: NUM_POINTS },
-  (_, k) => k + 1
-).map((num) => {
-  const randomRadius = randomFromInterval(MIN_RADIUS, MAX_RADIUS);
-  const randomAngle = Math.random() * Math.PI * 2;
+  () => {
+    const randomRadius = randomFromInterval(MIN_RADIUS, MAX_RADIUS);
+    const randomAngle = Math.random() * Math.PI * 2;
 
-  const x = Math.cos(randomAngle) * randomRadius;
-  const y = Math.sin(randomAngle) * randomRadius;
-  const z = randomFromInterval(-DEPTH, DEPTH);
+    const x = Math.cos(randomAngle) * randomRadius;
+    const y = Math.sin(randomAngle) * randomRadius;
+    const z = randomFromInterval(-DEPTH, DEPTH);
 
-  return {
-    idx: num,
-    position: [x, y, z],
-  };
-});
+    return {
+      position: [x, y, z],
+    };
+  }
+);
 
-export const pointsOuter = Array.from(
+export const pointsOuter: PointData[] = Array.from(
   { length: NUM_POINTS / 4 },
-  (_, k) => k + 1
-).map((num) => {
-  const randomRadius = randomFromInterval(MIN_RADIUS / 2, MAX_RADIUS * 2);
-  const angle = Math.random() * Math.PI * 2;
+  () => {
+    const randomRadius = randomFromInterval(MIN_RADIUS / 2, MAX_RADIUS * 2);
+    const angle = Math.random() * Math.PI * 2;
 
-  const x = Math.cos(angle) * randomRadius;
-  const y = Math.sin(angle) * randomRadius;
-  const z = randomFromInterval(-DEPTH * 10, DEPTH * 10);
+    const x = Math.cos(angle) * randomRadius;
+    const y = Math.sin(angle) * randomRadius;
+    const z = randomFromInterval(-DEPTH * 10, DEPTH * 10);
 
-  return {
-    idx: num,
-    position: [x, y, z],
-  };
-});
+    return {
+      position: [x, y, z],
+    };
+  }
+);
